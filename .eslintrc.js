05040a12a445707df3ffa542e5e8598c8329058d
Eleonora Prokushenkova <eleonoraprokushenkova@MacBook-Air-Eleonora.local>
2022-05-08 11:43:58 +0300

    формы часть-1

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+
+    rules: {
+        indent: ["off", 4],
+        semi: [2, "always"],
+        "space-before-function-paren": [
+            "error",
+            { anonymous: "always", named: "never" }
+        ],
+        quotes: [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true
+            }
+        ]
+    },
+    plugins: ["react", "prettier"],
+    extends: [
+        "plugin:react/recommended",
+        "standard",
+        "plugin:prettier/recommended"
+    ]
+};
